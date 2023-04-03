80e8dd830573ca568a671468a1a8d629174f5f6a
Aaron M. Agosto <aaronagosto0613@gmail.com>
2023-04-03 11:56:02 -0400

    built simple api

---
+++ index.js
@@ -0,0 +1,21 @@
+import express from "express" 
+
+
+const PORT = 3000
+
+const app = express()
+
+
+// list of allowed request:
+app.get('/hello', (req, res) =>{
+    res.send("Hello there")
+})
+
+app.get('/test', (req, res) =>{
+    res.send("it's working! <3")
+})
+
+
+app.listen(PORT, () => {
+    console.log(`Listening on http://localhost:${PORT}...`)
+})
